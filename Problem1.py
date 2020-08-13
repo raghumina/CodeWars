d83f98d9051c3901cb6697312212289e1063166c
raghu <raghumina11@gmail.com>
2020-08-13 22:46:52 +0530

    Create Problem1.py

---
+++ Problem1.py
@@ -0,0 +1,6 @@
+# It should remove all values from list a, which are present in list b.
+#
+# array_diff([1,2],[1]) == [2]
+# If a value is present in b, all of its occurrences must be removed from the other:
+#
+# array_diff([1,2,2,2,3],[2]) == [1,3]
